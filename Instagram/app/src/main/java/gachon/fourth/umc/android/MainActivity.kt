07dc20f05d96cf21fbc93f48e700b532e0731efc
Nahyun Kim <hedghog1004@naver.com>
2023-11-22 23:56:28 +0900

    ✅ Test: JDBC driver를 이용한 MySQL 연결 테스트

--- Instagram/app/src/main/java/gachon/fourth/umc/android/MainActivity.kt
+++ Instagram/app/src/main/java/gachon/fourth/umc/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package gachon.fourth.umc.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import gachon.third.umc.android.ProfileFragment
@@ -8,6 +9,11 @@ import gachon.third.umc.android.R
 import gachon.third.umc.android.ReelsFragment
 import gachon.third.umc.android.ShopFragment
 import gachon.third.umc.android.databinding.ActivityMainBinding
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.sql.Statement
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +28,9 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
         // 바텀네비 아이템 클릭 이벤트 정의
         setBottomNavi()
+
+        // DB 데이터 조회 테스트
+        dbTest2()
     }
 
     private fun setBottomNavi() {
@@ -56,5 +65,117 @@ class MainActivity : AppCompatActivity() {
     private fun setFragment(fragment: Fragment) {
         // 이동할 Fragment 지정
         supportFragmentManager.beginTransaction().replace(R.id.main_frm, fragment).commit()
+    }
+
+    private fun dbTest2() {
+        val thread = Thread{
+            var con: Connection? = null
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver")
+                val url = resources.getString(R.string.url)
+                val user = resources.getString(R.string.user)
+                val passwd = resources.getString(R.string.password)
+                con = DriverManager.getConnection(url, user, passwd)
+                Log.d("Database", con.toString())
+            } catch (e: ClassNotFoundException) {
+                e.printStackTrace()
+            } catch (e: SQLException) {
+                e.printStackTrace()
+            }
+            // database operation
+            var stmt: Statement? = null
+            var rs: ResultSet? = null
+            try {
+                if (con != null) {
+                    stmt = con.createStatement()
+                    val sql = "select * from user"
+                    rs = stmt.executeQuery(sql)
+                    while (rs.next()) {
+                        var name = rs.getString(1)
+                        if (rs.wasNull()) name = "null"
+                        var course_id = rs.getString(2)
+                        if (rs.wasNull()) course_id = "null"
+                        println(name + "\t" + course_id)
+                    }
+                }
+            } catch (e1: SQLException) {
+                e1.printStackTrace()
+            }
+            try {
+                if (stmt != null && !stmt.isClosed()) stmt.close()
+            } catch (e1: SQLException) {
+                e1.printStackTrace()
+            }
+        }
+
+        thread.start()
+        try {
+            thread.join()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun dbTest() {
+        val thread = Thread{
+            val connection = connectToDatabase()
+
+            if (connection != null) {
+                try {
+                    val users = getAllUsers(connection)
+                    for (user in users) {
+                        Log.d("Database", user.toString())
+                    }
+                } finally {
+                    // 연결 해제
+                    connection.close()
+                }
+            } else {
+                Log.d("Database", "Failed to connect to the database.")
+            }
+        }
+        thread.start()
+    }
+
+    fun connectToDatabase(): Connection? {
+        val url = resources.getString(R.string.url)
+        val user = resources.getString(R.string.user)
+        val password = resources.getString(R.string.password)
+
+        return try {
+            DriverManager.getConnection(url, user, password)
+        } catch (e: SQLException) {
+            null
+        }
+    }
+
+    fun getAllUsers(connection: Connection): List<User> {
+        val sql = "SELECT * FROM user"
+
+        return try {
+            val statement = connection.createStatement()
+            val resultSet = statement.executeQuery(sql)
+
+            val users = ArrayList<User>()
+
+            while (resultSet.next()) {
+                val id = resultSet.getInt("user_id")
+                val userName = resultSet.getString("user_name")
+                val password = resultSet.getString("password")
+                val name = resultSet.getString("name")
+                val email = resultSet.getString("email")
+
+                val user = User(id, userName, password, name, email)
+                users.add(user)
+            }
+
+            users
+        } catch (e: SQLException) {
+            println("An error occurred while executing the SQL query: $sql")
+            println(e.message)
+
+            ArrayList()
+        }
     }
 }
---
+++ Instagram/app/src/main/java/gachon/fourth/umc/android/User.kt
@@ -0,0 +1,14 @@
+package gachon.fourth.umc.android
+
+import java.time.LocalDateTime
+
+data class User (
+    val userId: Int,
+    val userName: String,
+    val password: String,
+    val name: String,
+    val email: String,
+//    val birthday: LocalDateTime,
+//    val gender: String,
+//    val introduction: String
+)
