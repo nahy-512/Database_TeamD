98d7d23b24e0fc5720a3e85a92f7ddaa35e9e05f
Nahyun Kim <hedghog1004@naver.com>
2023-11-23 15:27:52 +0900

    ✨ Feat: 사람 찾아보기 (팔로우 추천) 리사이클러뷰 구현

--- Instagram/app/src/main/java/gachon/database/instagram/ui/profile/ProfileFragment.kt
+++ Instagram/app/src/main/java/gachon/database/instagram/ui/profile/ProfileFragment.kt
@@ -2,18 +2,32 @@ package gachon.database.instagram.ui.profile
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import gachon.database.instagram.R
+import gachon.database.instagram.data.Follow
 import gachon.database.instagram.databinding.FragmentProfileBinding
 import gachon.database.instagram.ui.MainActivity
 import gachon.database.instagram.ui.profile.follow.FollowFragment
+import gachon.database.instagram.ui.profile.follow.adapter.RecommendFollowRVAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.sql.Connection
+import java.sql.SQLException
 
 class ProfileFragment: Fragment() {
 
     lateinit var binding: FragmentProfileBinding
+    private lateinit var adapter: RecommendFollowRVAdapter
+
+    private var isShowRecommend = false
+    private var recommends = ArrayList<Follow>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +38,16 @@ class ProfileFragment: Fragment() {
 
         // 클릭 이벤트 정의
         initClickListener()
+        // 초기 뷰 세팅
+        setInit()
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        getDatabaseData()
     }
 
     private fun initClickListener() {
@@ -43,6 +65,91 @@ class ProfileFragment: Fragment() {
             // 팔로잉 화면으로 이동
             moveToFollowFragment(false)
         }
+
+        binding.profileRecommendFollowIv.setOnClickListener {
+            //TODO: 이미지 변경
+            if (isShowRecommend) { // 펼쳐진 상태
+                binding.profileRecommendFollowCl.visibility = View.GONE
+            } else { // 닫혀있는 상태
+                binding.profileRecommendFollowCl.visibility = View.VISIBLE
+            }
+            // 접힘 상태 업데이트
+            isShowRecommend = !isShowRecommend
+        }
+    }
+
+    private fun setInit() {
+        binding.profileRecommendFollowCl.visibility = if (isShowRecommend) View.VISIBLE else View.GONE
+    }
+
+    private fun getDatabaseData() {
+        GlobalScope.launch(Dispatchers.IO) {
+            if (activity is MainActivity) {
+                val activity = activity as MainActivity
+                val connection = activity.connectToDatabase()
+
+                if (connection != null) {
+                    connection.use { connection ->
+                        // DB에서 조회한 팔로워 or 팔로잉 정보를 리스트로 가져오기
+                        val users = getAllFollowList(connection)
+
+                        // UI 업데이트를 메인 스레드에서 수행
+                        withContext(Dispatchers.Main) {
+                            recommends.clear()
+                            recommends.addAll(users)
+
+                            // 팔로워 or 팔로잉 리스트로 리사이클러뷰 데이터 초기화
+                            initRecommendRV()
+                        }
+                    }
+                } else {
+                    Log.d("Database", "Failed to connect to the database.")
+                }
+            }
+        }
+    }
+
+    fun getAllFollowList(connection: Connection): List<Follow> {
+        //TODO: 내가 팔로잉하지 않는 유저를 추천받아서 조회
+        val query = "SELECT * FROM user"
+
+        return try {
+            // Statement 객체를 생성하여 SQL 쿼리를 실행하기 위한 준비를 함
+            val statement = connection.createStatement()
+            // SQL SELECT 쿼리를 실행하고, 조회 결과를 테이블 형식의 데이터인 ResultSet 객체에 저장함
+            val resultSet = statement.executeQuery(query)
+
+            // 조회 결과를 반환할 리스트 (추천 유저 목록)
+            val recommends = ArrayList<Follow>()
+
+            while (resultSet.next()) { // 조회 결과를 한줄 한줄 받아옴
+                val id = resultSet.getInt("user_id")
+                val userName = resultSet.getString("user_name")
+                val name = resultSet.getString("name")
+                // 한 유저 인스턴스에 받아온 필드를 차례대로 넣어줌
+                val user = Follow(id, userName, name)
+                // 리스트에 위에서 받아온 유저 정보를 추가해줌
+                recommends.add(user)
+            }
+            // DB에서 조회한 추천 유저 리스트를 반환
+            recommends
+        } catch (e: SQLException) {
+            println("An error occurred while executing the SQL query: $query")
+            println(e.message)
+
+            ArrayList()
+        }
+    }
+
+    private fun initRecommendRV() {
+        // 리사이클러뷰 연결
+        adapter = RecommendFollowRVAdapter()
+        binding.profileRecommendFollowRv.adapter = adapter
+        binding.profileRecommendFollowRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        // 쿼리문으로 조회한 팔로워 or 팔로잉 리스트를 리사이클러뷰에 집어넣기
+        Log.d("ProfileFragment", "From database: $recommends")
+        adapter.addRecommendList(recommends)
     }
 
     private fun moveToEditActivity() {
---
+++ Instagram/app/src/main/java/gachon/database/instagram/ui/profile/follow/adapter/RecommendFollowRVAdapter.kt
@@ -0,0 +1,42 @@
+package gachon.database.instagram.ui.profile.follow.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import gachon.database.instagram.data.Follow
+import gachon.database.instagram.databinding.ItemRecommendFollowBinding
+
+class RecommendFollowRVAdapter : RecyclerView.Adapter<RecommendFollowRVAdapter.ViewHolder>() {
+
+    private var users = ArrayList<Follow>()
+
+    fun addRecommendList(follows: ArrayList<Follow>) {
+        this.users.clear()
+        this.users.addAll(follows)
+
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecommendFollowRVAdapter.ViewHolder {
+        val binding: ItemRecommendFollowBinding = ItemRecommendFollowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecommendFollowRVAdapter.ViewHolder, position: Int) {
+        holder.bind(users[position])
+    }
+
+    override fun getItemCount(): Int = users.size
+
+    inner class ViewHolder(val binding: ItemRecommendFollowBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(follower: Follow) {
+            binding.itemFollowRecommendUserNameTv.text = follower.userName
+//            binding.itemFollowRecommendDescTv.text = follower.name
+        }
+    }
+}
